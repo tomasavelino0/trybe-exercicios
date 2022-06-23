446b6f5c14360a0912d3ea86efb95af52959f506
tomas <tomasdc2016@gmail.com>
2022-06-22 22:34:43 -0300

    exercicios 8.1

---
+++ Fundamentos-do-desenvolvimento-web/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,54 @@
+//Exercicio 1
+
+const nameForEmail = (fullname) => {
+  return `${fullname.replace(' ', '_')}@trybe.com`
+}
+
+const newEmployees = () => {
+  const employees = {
+    id1: nameForEmail('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id3: nameForEmail('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id2: nameForEmail('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+// Exercicio 2
+
+const sortNumbers = () => {
+  let result = Math.floor(Math.random() * 5) + 1;
+  return result;
+};
+
+const resultadoSorteio = (num) => {
+  if (num === sortNumbers()) {
+    return console.log('Parabéns você ganhou');
+  } else {
+    return console.log('Tente novamente');
+  };
+};
+
+//exercicio 3
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const sumGrade = (rightAnswers, wrongAnswers) => {
+  let grade = 0;
+  for (let i = 0; i < RIGHT_ANSWERS.length; i += 1) {
+    if (RIGHT_ANSWERS[i] === STUDENT_ANSWERS[i]) {
+      grade += 1
+    } else if (STUDENT_ANSWERS[i] === 'N.A') {
+      grade += 0;
+    } else if (RIGHT_ANSWERS !== STUDENT_ANSWERS[i]) {
+      grade -= 0.5;
+    }
+  }
+  return grade;
+}
+
+const gradeResult = (rightAnswers, wrongAnswers, sumGrade) => {
+  return `Sua nota e de ${sumGrade(rightAnswers, wrongAnswers)} pontos.`
+}
+
+console.log(gradeResult(RIGHT_ANSWERS, STUDENT_ANSWERS, sumGrade));
